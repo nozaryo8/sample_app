b889041076abe60a9196d91c05d3986507e681d3
Ubuntu <ubuntu@ip-172-31-31-148.ap-northeast-1.compute.internal>
2021-01-14 16:38:25 +0000

    Add hello

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Cloud9 への接続を許可する
+  config.hosts.clear
+  
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?

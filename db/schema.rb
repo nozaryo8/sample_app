c46dcdeb312e0f656d05d185965a6cd5a6210043
Ubuntu <ubuntu@ip-172-31-31-148.ap-northeast-1.compute.internal>
2021-01-22 15:27:59 +0000

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_073005) do
+ActiveRecord::Schema.define(version: 2021_01_22_055239) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
